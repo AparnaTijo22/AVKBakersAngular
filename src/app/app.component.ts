286ab003e487737f73be349e6437ac4d9f1dfb74
AparnaTijo22 <aparnatijo22@gmail.com>
2023-11-05 21:50:34 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { DatePipe } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'restrauntAngular';
+  
+  today = new Date();
+  pipe = new DatePipe('en-US');
+  ChangedFormat = this.pipe.transform(this.today, 'YYYY-MM-dd');
+
+  navbarfixed:boolean = false;
+
+  @HostListener('window:scroll',['$event']) onscroll(){
+    if(window.scrollY > 100)
+    {
+      this.navbarfixed = true;
+    }
+    else
+    {
+      this.navbarfixed = false;
+    }
+  }
+}
